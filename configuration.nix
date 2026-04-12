d06d3a70974f1058f8fd8587b3d8716c906a36a6
huybin1234b <officalhuybin1234b@duck.com>
2026-04-12 12:11:52 +0700

    fuck the dns

--- configuration.nix
+++ configuration.nix
@@ -144,7 +144,10 @@
 
   networking.hostName = "huybin1234b-vivobook"; # Define your hostname.
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
-
+  networking.nameservers = [
+    "1.1.1.1"
+    "1.0.0.1"
+  ];
   # Configure network proxy if necessary
   # networking.proxy.default = "http://user:password@proxy:port/";
   # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
